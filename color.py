97e30b86a0ea3ddc6cc8d91d7e9665c7c2314042
facm1999 <34148939+facm1999@users.noreply.github.com>
2018-04-08 15:26:06 +0800

    Add files via upload

---
+++ color.py
@@ -0,0 +1,12 @@
+
+def blue(a):
+    return '\033[34m'+a
+def red(a):
+    return '\033[31m'+a
+def yellow(a):
+    return '\033[33m'+a
+def purple(a):
+    return '\033[35m'+a
+def cyan(a):
+    return '\033[36m'+a
+
